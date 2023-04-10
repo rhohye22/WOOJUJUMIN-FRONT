6e2b37ca25002942f320dfc880fa4226144437e2
calendar2 <zoro94317@gmail.com>
2023-04-10 17:30:18 +0900

    우주주민 main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import Login from "./components/login";
+import Main from "./components/main";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <header>
+          <Link to="/">우주주민</Link>
+        </header>
+        <hr/>
+
+        <nav>
+          <h1>여기 네비바</h1>
+          <Link to="/login">Login</Link> &nbsp;&nbsp;
+         
+        </nav>
+        <hr/>
+
+        <main>
+          <h1>여기 메인</h1>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Login />} />
+            
+            
+        
+          </Routes>
+        </main>
+        <hr/>
+      </BrowserRouter>
+
+      <footer>
+        <h1>여긴 푸터</h1>
+      </footer>
     </div>
   );
 }
