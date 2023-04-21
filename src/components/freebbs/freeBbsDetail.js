ad8bc92d49756f60f9f7e46604a86b7f713ce85a
rhohye22 <nhw666@gmail.com>
2023-04-21 19:49:26 +0900

    freebbs댓글

--- src/components/freebbs/freeBbsDetail.js
+++ src/components/freebbs/freeBbsDetail.js
@@ -1,10 +1,25 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-
+import FreeBbsReply from "./freeBbsReply";
 function FreeBbsDetail() {
+  let navigate = useNavigate();
+
   const [freebbs, setFreeBbs] = useState({});
   const [loading, setLoading] = useState(false);
+
+  //댓글
+  const [replySeq, setReplySeq] = useState();
+  const [content, setContent] = useState("");
+  const [writer, setWriter] = useState("");
+
+  //접속정보
+  useEffect(function () {
+    let login = JSON.parse(localStorage.getItem("login"));
+    setWriter(login.id);
+  }, []);
+
   let params = useParams();
   const qnaData = async (bbsSeq) => {
     const response = await axios.get("http://localhost:3000/getfreeBbs", {
@@ -17,6 +32,7 @@ function FreeBbsDetail() {
 
   useEffect(() => {
     qnaData(params.bbsSeq);
+    setReplySeq(params.bbsSeq);
   }, [params.bbsSeq]);
 
   if (loading === false) {
@@ -25,9 +41,40 @@ function FreeBbsDetail() {
 
   const imageUrl = freebbs.image !== null ? `http://localhost:3000/upload/freebbs/${freebbs.image.substring(66)}` : null;
 
+  const contentChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  //댓글쓰기
+  function writeFreebbsReply() {
+    if (content === undefined || content.trim() === "") {
+      alert("댓글 내용을 입력해 주십시오");
+      return;
+    }
+
+    axios
+      .post("http://localhost:3000/writeFreeReply", null, {
+        params: { writer: writer, replySeq: replySeq, content: content },
+      })
+      .then((res) => {
+        console.log(res.data);
+        if (res.data === "YES") {
+          alert("성공적으로 등록되었습니다");
+          navigate(`/freeBbsDetail/${replySeq}`);
+          setContent("");
+        } else {
+          alert("등록되지 않았습니다");
+        }
+      })
+      .catch(function (err) {
+        alert(err);
+      });
+  }
+
   return (
     <div>
       <h2>제목 : {freebbs.title}</h2>
+      <h2>작성자 : {freebbs.id}</h2>
       {imageUrl !== null ? (
         <img
           src={imageUrl}
@@ -41,6 +88,17 @@ function FreeBbsDetail() {
         />
       ) : null}
       <p>내용 : {freebbs.content}</p>
+      <br />
+      <FreeBbsReply replySeq={replySeq} />
+      <br />
+      <label>
+        댓글내용
+        <input type="text" value={content} onChange={contentChange} />
+      </label>
+      <button type="submit" onClick={writeFreebbsReply}>
+        작성하기
+      </button>
+      <br /> <br /> <br />
     </div>
   );
 }
