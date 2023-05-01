706ff8d7e4c788b0ee7dd71caeb2c31585287d9d
maximilian012 <99068583+maximilian012@users.noreply.github.com>
2023-05-01 09:54:49 +0900

    5/1

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -11,11 +11,11 @@ const Search = () => {
   const {currentUser} = useContext(AuthContext);
 
   const handleSearch = async () =>{
-    const q = query(collection(db, "users"),
-    where("displayName", "==", username));
+    const q = query(collection(db, "users"),// db users라는 컬렉션에서 조건이 displayname과 username이 같은거
+    where("displayName", "==", username)); // 컬렉션, 컬렉션 그룹에서 검색할 문서를 지정하는 쿼리문
 
     try{
-      const querySnapshot = await getDocs(q);
+      const querySnapshot = await getDocs(q); // getDocs로 row가져오기 컬렉션의 여러문서
       //alert(JSON.stringify(q));
       querySnapshot.forEach((doc) => {
         setUser(doc.data())  
@@ -35,18 +35,19 @@ const Search = () => {
 
   const handleSelect = async () =>{
     // check whether the group(chats in firestore) exists, if not create new one
+    // 1대1채팅이므로 상대아이디와 내아이디만의 채팅방을 나타내는 id가 필요 그래서 합침
     const combinedId = currentUser.uid > user.uid ? currentUser.uid + user.uid : user.uid + currentUser.uid;
 
     try{
-
-      const res = await getDoc(doc(db, "chats", combinedId));
+      
+      const res = await getDoc(doc(db, "chats", combinedId));// get()을 이용해서 단일문서의 내용 검색
 
       if(!res.exists()){
         // create a chat in chats collection
-        await setDoc(doc(db,"chats", combinedId), { messages: []});
+        await setDoc(doc(db,"chats", combinedId), { messages: []});// 필드명 : messages
 
         // create user chats
-        await updateDoc(doc(db, "userChats", currentUser.uid),{
+        await updateDoc(doc(db, "userChats", currentUser.uid),{ // 내가 보낸거
           [combinedId + ".userInfo"]: {
             uid: user.uid,
             displayName: user.displayName,
@@ -54,7 +55,7 @@ const Search = () => {
           },
           [combinedId + ".date"]: serverTimestamp()
         });
-        await updateDoc(doc(db, "userChats", user.uid),{
+        await updateDoc(doc(db, "userChats", user.uid),{ // 상대가 보낸거
           [combinedId + ".userInfo"]: {
             uid: currentUser.uid,
             displayName: currentUser.displayName,
