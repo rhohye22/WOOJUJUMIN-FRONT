4aa6532d608b322f156de5438f912683548a6b54
coke709 <112848935+coke709@users.noreply.github.com>
2023-05-12 15:26:49 +0900

    5월 12일 오후 3시30분

--- src/components/main/partycardBtn.js
+++ src/components/main/partycardBtn.js
@@ -25,7 +25,7 @@ function PartycardBtn() {
         <div className="partycontent"></div>
       </div>
       <div className="partycard">
-        <img alt="recommemd" src="http://images.munto.kr/production-socialing/1683600044434-photo-9besu-426683-0?s=256x256" width="130" height="130" style={{ borderRadius: "30px" }}></img>
+        <img alt="recommemd" src="http://images.munto.kr/production-socialing/1683600044434-photo-9besu-426683-0?s=256x256" width="130" height="130" style={{ borderRadius: "30px" }}></img>       
         <div className="partycontent"></div>
       </div>
     </>
